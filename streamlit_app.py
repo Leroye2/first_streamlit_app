0c97190c2d6950a86733267f0582a044c277f934
Leroye2 <152780290+Leroye2@users.noreply.github.com>
2023-12-03 18:21:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text(' 🐔 🥑🍞Kale, Spinach & Rocket Smoothie')
-streamlit.text(' 🥑🍞Hard-Boiled Free-Range Egg')
+streamlit.text('🐔🥑🍞Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
